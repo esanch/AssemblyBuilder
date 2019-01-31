abc4ef3305a02f5c1455b38b52cd4694bc2d365a
Elizabeth Earl <Elizabeth.Earl@ubeusa.com>
2019-01-31 11:42:07 -0800

    Read Only

--- InvoiceAdd.cs
+++ InvoiceAdd.cs
@@ -167,6 +167,7 @@ namespace InvoiceAdd
             this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle2;
             this.dataGridView1.Location = new System.Drawing.Point(12, 89);
             this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
             dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
             dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control;
             dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
